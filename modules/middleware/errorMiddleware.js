ee27c3fb77df8ffff89a401954efeec2d1b426e8
effs12 <fredericklopez12@gmail.com>
2022-08-11 02:24:02 -0400

    Update the errorMiddleware status code handler

--- modules/middleware/errorMiddleware.js
+++ modules/middleware/errorMiddleware.js
@@ -1,5 +1,6 @@
 const errorHandler = (err, req, res, next) => {
-    const statusCode = res.statusCode === 200 ?  500 : res.statusCode
+    const statusCode = res.statusCode ? res.statusCode : 500
+    
     res.status(statusCode)
 
     res.json({
