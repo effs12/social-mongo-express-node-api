4ae0f8aa5a9fd6ce9dba0cc644611946e531e37b
effs12 <fredericklopez12@gmail.com>
2022-08-10 20:34:29 -0400

    Added the jwt token, encryption and User registration and Login validation

--- modules/controllers/userController.js
+++ modules/controllers/userController.js
@@ -1,4 +1,7 @@
 import asyncHandler from 'express-async-handler'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import User from '../models/userModel.js'
 
 // @desc    User Registration 
 // @route   POST /api/users
@@ -11,14 +14,55 @@ const registerUser = asyncHandler(async(req, res) => {
         throw new Error('Please add all fields')
     }
 
-    res.status(200).json({ message : 'Register New User'})
+    //Check if User Exist 
+    const userExist = await User.findOne({username})
+    
+    if(userExist){
+        res.status(400)
+        throw new Error('Username already registered')
+    }
+
+    //Hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    //Create user
+    const user = await User.create({
+        username,
+        password : hashedPassword,
+    })
+
+    if(user){
+        res.status(201).json({
+            _id : user.id,
+            username : user.username,
+            token: generateToken(user._id),
+        })
+    }else{
+        res.status(400)
+        throw new Error(`Invalid user data`)
+    }
 })
 
 // @desc    User Authentication
 // @route   POST /api/users/login
 // @access  Public
 const loginUser = asyncHandler(async(req, res) => {
-    res.status(200).json({ message : 'Login User'})
+    const { username, password } = req.body
+
+    //Validate if username 
+    const user = await User.findOne({username})
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            _id : user.id,
+            username : user.username,
+            token : generateToken(user._id)
+        })
+    }else{
+        res.status(400)
+        throw new Error(`Invalid credentials`)
+    }
 })
 
 // @desc    User Account data
@@ -27,6 +71,13 @@ const loginUser = asyncHandler(async(req, res) => {
 const getAccount= asyncHandler(async(req, res) => {
     res.status(200).json({ message : 'User Account'})
 })
+
+// Generate JWT token
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    })
+}
 
 export {
     registerUser,
