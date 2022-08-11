ca83ae83634d497d71b6cf774caecb2a6b636070
effs12 <fredericklopez12@gmail.com>
2022-08-11 01:01:06 -0400

    Create the Tweet Create, Get, Edit, Delete functionality

--- modules/controllers/tweetController.js
+++ modules/controllers/tweetController.js
@@ -1,31 +1,63 @@
 import asyncHandler from 'express-async-handler'
+import Tweet from '../models/tweetModel.js'
 
 // @desc    Get all User tweets
 // @route   GET /api/tweets/
 // @access  Private
 const getTweets = asyncHandler(async (req, res) => {
-    res.status(200).json({ message : 'Get All User Tweets!!'})
+    const tweets = await Tweet.find()
+
+    res.status(200).json(tweets)
 })
 
 // @desc    Make Tweet
 // @route   POST /api/tweets
 // @access  Private
 const makeTweet = asyncHandler(async (req, res) => {
-    res.status(200).json({ message : 'Make a Tweet!!'})
+    if (!req.body.content) {
+        res.status(400)
+        throw new Error('Please add a text field')
+    }
+
+    const tweet = await Tweet.create({
+        content: req.body.content,
+    })
+    
+    res.status(200).json(tweet)
 })
 
 // @desc    Edit/Update Tweet
 // @route   POST /api/tweets/:id
 // @access  Private
 const editTweet = asyncHandler(async (req, res) => {
-    res.status(200).json({ message : 'Edit this Tweet!!'})
+    const tweet = await Tweet.findById(req.params.id)
+
+    if (!tweet) {
+      res.status(400)
+      throw new Error('Tweet does not exist')
+    }
+
+    const editTweet = await Tweet.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    })
+    
+    res.status(200).json(editTweet)
 })
 
 // @desc    Delete Tweet
 // @route   POST /api/tweets/:id
 // @access  Private
 const deleteTweet = asyncHandler(async (req, res) => {
-    res.status(200).json({ message : 'Delete this Tweet!!'})
+    const tweet = await Tweet.findById(req.params.id)
+
+    if (!tweet) {
+        res.status(400)
+        throw new Error('Tweet does not exist')
+    }
+
+    await tweet.remove()
+
+    res.status(200).json({ id: req.params.id })
 })
 
 export {
